2ce1a6a97294ead4a145f9d368a1a7343087e29a
Despa11400 <alexdespot007@gmail.com>
2025-02-26 05:17:54 +0100

    Update port configuration and fix MainController

--- src/main/java/com/theater/repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.theater.repository;
-
-import com.theater.model.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, String> {
-} 
--- src/main/java/com/theater/repository/SeatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.theater.repository;
-
-import com.theater.model.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatRepository extends JpaRepository<Seat, String> {
-} 
